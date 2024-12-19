64e4e25c42c0a8ea5432af993c30053541e5850a
joaopakina14 <pakina@live.com.pt>
2024-12-19 15:35:18 +0000

    sobreNos

--- WebQuote/src/Pages/SobreNos.jsx
+++ WebQuote/src/Pages/SobreNos.jsx
@@ -1,16 +1,29 @@
-import Header from '../Components/Header/Header'
+import Header from '../Components/Header/Header';
 import Footer from '../Components/Footer/Footer';
 import './SobreNos.css';
+import FotoSobreNos from '../assets/foto_SobreNosCesae.png';
 
 export default function SobreNos() {
   return (
     <>
-    <div>
-      <Header/>
-      <h1 className='sobrenosh1'>Sobre Nós</h1>
-      
-    </div>
-    <Footer />
+      <div className="sobrenos-container">
+        <Header />
+        <div className="sobrenos-content">
+          <br />
+          <br />
+          <h1 className="sobrenos-heading">Sobre Nós</h1>
+          <div className="sobrenos-main">
+            <img src={FotoSobreNos} alt="Sobre Nós" className="sobrenos-image" />
+            <p className="sobrenos-text">
+              "Bem-vindo à nossa empresa! Somos especializados na criação de websites e e-commerces sob medida, 
+              com foco em inovação, funcionalidade e design moderno. Nossa missão é transformar ideias em soluções digitais 
+              que impulsionem os negócios dos nossos clientes. Como empreendedores em nome individual, valorizamos a proximidade 
+              e a personalização no atendimento, garantindo soluções únicas para cada cliente."
+            </p>
+          </div>
+        </div>
+        <Footer />
+      </div>
     </>
   );
 }
