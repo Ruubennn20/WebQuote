8c46b624e5660936ef617689df71f3121d98c59d
Ruubennn20 <rubencouto2002@gmail.com>
2025-01-09 15:49:30 +0000

    nao dar merge isto é so CSS

--- WebQuote/src/Pages/Contactos.jsx
+++ WebQuote/src/Pages/Contactos.jsx
@@ -55,23 +55,32 @@ export default function Contactos() {
             <iframe
               src="https://www.google.com/maps?q=Rua%20de%20Fund%C3%B5es,%20S%C3%A3o%20Jo%C3%A3o%20da%20Madeira&z=15&output=embed"
               width="100%"
-              height="450"
+              height="600"
               style={{ border: 0 }}
               allowFullScreen=""
               loading="lazy"
               title="Mapa de Rua de Fundões São João da Madeira"
             />
-            <p className="map-text">Morada :</p>
-            <p className="map-text">
-              Rua de Fundões 151, 3700-121 São João da Madeira
-            </p>
+            <div className="contact-info-section">
+              <div className="contact-info-item">
+                <h3>Morada</h3>
+                <p>Rua de Fundões 151, 3700-121</p>
+                <p>São João da Madeira</p>
+              <p></p>
+              </div>
+              
+              <div className="contact-info-item">
+                <h3>Contactos Diretos</h3>
+                <p>Tel: +351 919 014 749</p>
+                <p>Email: geral.webquote@gmail.com</p>
+              </div>
+            </div>
           </div>
 
           {/* Formulário */}
           <div className="contact-form-container">
             <form className="contact-form" onSubmit={handleSubmit}>
               <h2>Deixe-nos a sua mensagem:</h2>
-              
               <label htmlFor="name">Nome:</label>
               <input 
                 type="text"
